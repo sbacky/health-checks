3d2c27a0a408106b5e0790d4f9fd186fc89dfb01
Neil <nhc.crum@gmail.com>
2022-09-22 01:14:11 -0700

    Init repo

---
+++ display_vitals.py
@@ -0,0 +1,37 @@
+#! usr/bin/env python3
+
+import psutil
+import shutil
+import os
+
+def cpu_usage():
+    """Returns CPU usage as percentage as string"""
+    # Getting loadover15 minutes
+    load1, load5, load15 = psutil.getloadavg()
+    cpu_usage = (load15/os.cpu_count()) * 100
+    return f'The CPU usage is : {cpu_usage}'
+
+def ram_usage():
+    """Returns RAM usage as percentage as string"""
+    # Getting % usage of virtual_memory ( 3rd field)
+    return f'RAM memory % used: {psutil.virtual_memory()[2]}'
+
+def storage_usage():
+    """Returns Storage usage as percetage as string"""
+    du = shutil.disk_usage("/")
+    # Calculate the percentage of free space
+    percent_used = 100 * du.used / du.total
+    percent_free = 100 * du.free / du.total
+    total_gb = du.total / 2**30
+    return f'Storage % used: {percent_used}\nStorage % free: {percent_free}\nTotal: {total_gb}gb'
+
+def main():
+    vitals = [
+        cpu_usage,
+        ram_usage,
+        storage_usage,
+    ]
+    for vital in vitals:
+        print(vital())
+
+main()
